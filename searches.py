03cd50d41d2de911e362cdd8cfb506b4a0a57c5f
freedomzx <lejongoco@gmail.com>
2021-02-02 00:05:43 -0500

    visualize BFS

--- searches.py
+++ searches.py
@@ -222,6 +222,124 @@ def findShortestBFS(maze, firstLocation, secondLocation):
     
     return -1
 
+#visualize BFS via pygame
+def visualizeBFS(maze, firstLocation, secondLocation):
+    dimen = len(maze)
+    width = 6
+    height = 6
+    margin = 0
+
+    pygame.init()
+    screen = pygame.display.set_mode([600, 600])
+    pygame.display.set_caption("DFS Visualization")
+    screen.fill(black)
+    clock = pygame.time.Clock()
+
+    for i in range(dimen):
+        for j in range(dimen):
+            color = white
+            if i == firstLocation[0] and j == firstLocation[1]:
+                color = green #color start green
+            elif i == secondLocation[0] and j == secondLocation[1]:
+                color = red #color goal red
+            elif maze[i][j] == 1:
+                color = black
+            pygame.draw.rect(screen,
+                            color,
+                            [(margin + width) * j + margin,
+                            (margin + height) * i + margin,
+                            width,
+                            height])
+            pygame.display.flip()
+
+    success = [False, []]
+    done = False
+    while not done:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:  
+                done = True 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                done = True
+
+        fringe = deque() #use a queue for BFS
+        first = [firstLocation[0], firstLocation[1], []]
+        fringe.append(first)
+        visited = []
+
+        while fringe:
+            current = fringe.popleft()
+            if current[0] == secondLocation[0] and current[1] == secondLocation[1]:
+                toReturn = []
+                for i in current[2]:
+                    toReturn.append(i)
+                toReturn.append([current[0], current[1]])
+                success = [True, toReturn]
+                break
+                
+            else:
+                if current not in visited:
+                    currentFirst = current[0]
+                    currentSecond = current[1]
+                    if currentFirst-1 >= 0 and currentFirst-1 < len(maze) and currentSecond >= 0 and currentSecond < len(maze):
+                        temp = []
+                        temp.append(currentFirst-1)
+                        temp.append(currentSecond)
+                        temp.append([])
+                        if current[2]:
+                            for i in current[2]:
+                                temp[2].append(i)
+                        temp[2].append([currentFirst, currentSecond])
+                        if maze[temp[0]][temp[1]] == 0 and [temp[0], temp[1]] not in visited:
+                            fringe.append(temp)
+                    if currentFirst >= 0 and currentFirst < len(maze) and currentSecond-1 >= 0 and currentSecond-1 < len(maze):
+                        temp = []
+                        temp.append(currentFirst)
+                        temp.append(currentSecond-1)
+                        temp.append([])
+                        if current[2]:
+                            for i in current[2]:
+                                temp[2].append(i)
+                        temp[2].append([currentFirst, currentSecond])
+                        if maze[temp[0]][temp[1]] == 0 and [temp[0], temp[1]] not in visited:
+                            fringe.append(temp)
+                    if currentFirst+1 >= 0 and currentFirst+1 < len(maze) and currentSecond >= 0 and currentSecond < len(maze):
+                        temp = []
+                        temp.append(currentFirst+1)
+                        temp.append(currentSecond)
+                        temp.append([])
+                        if current[2]:
+                            for i in current[2]:
+                                temp[2].append(i)
+                        temp[2].append([currentFirst, currentSecond])
+                        if maze[temp[0]][temp[1]] == 0 and [temp[0], temp[1]] not in visited:
+                            fringe.append(temp)
+                    if currentFirst >= 0 and currentFirst < len(maze) and currentSecond+1 >= 0 and currentSecond+1 < len(maze):
+                        temp = []
+                        temp.append(currentFirst)
+                        temp.append(currentSecond+1)
+                        temp.append([])
+                        if current[2]:
+                            for i in current[2]:
+                                temp[2].append(i)
+                        temp[2].append([currentFirst, currentSecond])
+                        if maze[temp[0]][temp[1]] == 0 and [temp[0], temp[1]] not in visited:
+                            fringe.append(temp)
+
+                    maze[current[0]][current[1]] = 3
+                    color = blue
+                    pygame.draw.rect(screen,
+                                color,
+                                [(margin + width) * current[1] + margin,
+                                (margin + height) * current[0] + margin,
+                                width,
+                                height])
+                    pygame.display.flip()
+                    visited.append([currentFirst, currentSecond])
+            
+    pygame.quit()
+    return success
+
+
 #heuristic that guesses distance based on euclidean dist formula
 def determineEuDist(firstLocation, secondLocation):
     return math.sqrt(math.pow(firstLocation[0] - secondLocation[0], 2) + math.pow(firstLocation[1] - secondLocation[1], 2))
@@ -336,8 +454,11 @@ def findShortestA(maze, firstLocation, secondLocation):
     
     #return empty list if it can't find the goal node from the start state
     return []
-#print(visualizeDFS(maze_generator(100, 0.15), [0, 0], [99, 99]))
-# print(checkPathDFS(testMaze, [0, 0], [3, 3]))
-# print(findShortestBFS(testMaze, [0, 0], [3, 3]))
-print(findShortestA(maze_generator(100, 0.3), [0, 0], [99, 99]))
+
+
+#print(visualizeDFS(maze_generator(100, 0.3), [0, 0], [99, 99]))
+#print(checkPathDFS(testMaze, [0, 0], [3, 3]))
+#print(findShortestBFS(testMaze, [0, 0], [3, 3]))
+#print(findShortestA(maze_generator(100, 0.3), [0, 0], [99, 99]))
 #print(determineEuDist([99, 98], [99, 99]))
+print(visualizeBFS(maze_generator(10, 0.3), [0, 0], [9, 9]))
