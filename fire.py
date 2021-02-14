9054fe3669edd10ac409347c591a64f223e4616a
freedomzx <lejongoco@gmail.com>
2021-02-13 23:13:24 -0500

    fire advance

--- fire.py
+++ fire.py
@@ -1,5 +1,34 @@
 from searches import *
 from mazeGenerator import maze_generator
+import random
+
+def advance_fire(maze, q):
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
+            #check if not obstacle and not on fire
+            if maze[i][j] != 3 and maze[i][j] != 1:
+                #check nu mof neighbors on fire
+                numOfFireChildren = 0
+                if checkValidChild(maze, i-1, j):
+                    if maze[i-1][j] == 3:
+                        numOfFireChildren += 1
+                
+                if checkValidChild(maze, i, j-1):
+                    if maze[i][j-1] == 3:
+                        numOfFireChildren += 1
+
+                if checkValidChild(maze, i+1, j):
+                    if maze[i+1][j] == 3:
+                        numOfFireChildren += 1
+
+                if checkValidChild(maze, i, j+1):
+                    if maze[i][j+1] == 3:
+                        numOfFireChildren += 1
+
+                probability = 1 - pow((1-q), numOfFireChildren)
+                probability *= 100
+                if random.randint(1, 100) <= probability:
+                    maze[i][j] = 3
 
 def strategyOne(maze):
     pass
