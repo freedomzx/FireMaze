a42189b50bab560d3c49bc0dbe4612fa44298893
xiaoxuan <chentina905@gmail.com>
2021-02-17 01:10:41 -0500

    visualization for strategy one

--- fire.py
+++ fire.py
@@ -1,7 +1,14 @@
 from searches import *
 from mazeGenerator import maze_generator
 import random
+import pygame
 
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+orange = (255, 165, 0)
 #advance fire in maze via algorithm given in pdf
 def advance_fire(maze, q):
     for i in range(len(maze)):
@@ -76,6 +83,157 @@ def strategyTwo(maze, q):
 def strategyThree(maze):
     pass
 
+def visualizeStrategyOne(maze, q):
+    dimen = len(maze)
+    width = 5
+    height = 5
+    margin = 0
+
+    pygame.init()
+    screen = pygame.display.set_mode([700, 700])
+    pygame.display.set_caption("DFS Visualization")
+    screen.fill(black)
+    clock = pygame.time.Clock()
+
+    for i in range(dimen):
+        for j in range(dimen):
+            color = white
+            if i == maze[0][0] and j == maze[0][0]:
+                color = green #color start green
+            elif i == maze[len(maze)-1][len(maze)-1] and j == maze[len(maze)-1][len(maze)-1]:
+                color = red #color goal red
+            elif maze[i][j] == 1:
+                color = black
+            elif maze[i][j] == 5: #color fire orange
+                color = orange
+            pygame.draw.rect(screen,
+                            color,
+                            [(margin + width) * j + margin,
+                            (margin + height) * i + margin,
+                            width,
+                            height])
+            
+    pygame.display.flip()
+    success = False
+    done = False
+    while not done:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:  
+                done = True 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                done = True
+        shortestPath = []
+        curr = [0, 0]
+        shortestPath = findShortestBFS(maze, curr, [len(maze)-1, len(maze)-1])
+        if shortestPath[0] == 'No path': 
+            print(-2)
+        for i in range(len(shortestPath)):
+            curr = shortestPath[i]
+            print(curr)
+            if maze[curr[0]][curr[1]] == 5:
+                print('-1')
+            if maze[curr[0]][curr[1]] == maze[len(maze)-1][len(maze)-1]:
+                print('200')
+            advance_fire(maze, q)
+            maze[curr[0]][curr[1]] = 3
+            color = blue
+            pygame.draw.rect(screen,
+                                    color,
+                                    [(margin + width) * curr[1] + margin,
+                                    (margin + height) * curr[0] + margin,
+                                    width,
+                                    height])
+            pygame.display.flip()           
+    pygame.quit()
+
+def visualizeStrategyTwo(maze, q):
+    dimen = len(maze)
+    width = 5
+    height = 5
+    margin = 0
+
+    pygame.init()
+    screen = pygame.display.set_mode([700, 700])
+    pygame.display.set_caption("DFS Visualization")
+    screen.fill(black)
+    clock = pygame.time.Clock()
+
+    for i in range(dimen):
+        for j in range(dimen):
+            color = white
+            if i == firstLocation[0] and j == firstLocation[1]:
+                color = green #color start green
+            elif i == secondLocation[0] and j == secondLocation[1]:
+                color = red #color goal red
+            elif maze[i][j] == 1:
+                color = black
+            pygame.draw.rect(screen,
+                            color,
+                            [(margin + width) * j + margin,
+                            (margin + height) * i + margin,
+                            width,
+                            height])
+            
+    pygame.display.flip()
+
+    success = False
+    done = False
+    while not done:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:  
+                done = True 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                done = True
+
+        fringe = deque() #use a stack for DFS
+        fringe.append((firstLocation[0], firstLocation[1]))
+        visited = set()
+        while fringe:
+            current = fringe.pop()
+            if current[0] == secondLocation[0] and current[1] == secondLocation[1]: #if found goal then get out of here
+                success = True
+                break
+            else:
+                if current not in visited:
+                    currentFirst = current[0]
+                    currentSecond = current[1]
+                    #check 4 neighbors to see which we can add to fringe
+                    if currentFirst-1 >= 0 and currentFirst-1 < len(maze) and currentSecond >= 0 and currentSecond < len(maze): #up
+                        temp = (currentFirst-1, currentSecond)
+
+                        if maze[temp[0]][temp[1]] == 0 and temp not in visited:
+                            fringe.append(temp)
+                    if currentFirst >= 0 and currentFirst < len(maze) and currentSecond-1 >= 0 and currentSecond-1 < len(maze): #left
+                        temp = (currentFirst, currentSecond-1)
+
+                        if maze[temp[0]][temp[1]] == 0 and temp not in visited:
+                            fringe.append(temp)
+                    if currentFirst+1 >= 0 and currentFirst+1 < len(maze) and currentSecond >= 0 and currentSecond < len(maze): #down
+                        temp = (currentFirst+1, currentSecond)
+
+                        if maze[temp[0]][temp[1]] == 0 and temp not in visited:
+                            fringe.append(temp)
+                    if currentFirst >= 0 and currentFirst < len(maze) and currentSecond+1 >= 0 and currentSecond+1 < len(maze): #right
+                        temp = (currentFirst, currentSecond+1)
+
+                        if maze[temp[0]][temp[1]] == 0 and temp not in visited:
+                            fringe.append(temp)
+
+                    maze[current[0]][current[1]] = 3
+                    color = blue
+                    pygame.draw.rect(screen,
+                                color,
+                                [(margin + width) * current[1] + margin,
+                                (margin + height) * current[0] + margin,
+                                width,
+                                height])
+                    pygame.display.flip()
+                    visited.add(current)
+                    
+    pygame.quit()
+    return success
+        
+
 testMaze = [[0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0],
 [0 , 0, 0, 0, 0],
@@ -83,10 +241,12 @@ testMaze = [[0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0]]
 
 start_fire(testMaze)
-print(testMaze)
+#print(testMaze)
 advance_fire(testMaze, 0.3)
-print(testMaze)
+#print(testMaze)
 advance_fire(testMaze, 0.3)
-print(testMaze)
+#print(testMaze)
 advance_fire(testMaze, 0.3)
-print(testMaze)
+#print(testMaze)
+print(visualizeStrategyOne(testMaze, 0.1))
+advance_fire(testMaze, 0.1)
